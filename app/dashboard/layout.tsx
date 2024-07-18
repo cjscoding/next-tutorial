5d0cbe12afec476afdf4bca98155d3510393141d
최지수 <sue@qoo10.com>
2024-07-18 17:59:42 +0900

    Initial Commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import NavLinks from "../ui/dashboard/nav-links";
+
+export default function Layout() {
+  return (
+    <div>
+      <NavLinks />
+      <div className="h-40"></div>
+    </div>
+  );
+}
